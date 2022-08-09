e6237cf681cd154ae1c1285d47a4d25c47d2bb11
vishal-gl <vishal.rathore@globallogic.com>
2022-08-09 17:51:25 +0530

    Updated

--- Day20/Linq_XML_Sort_Demo/Program.cs
+++ Day20/Linq_XML_Sort_Demo/Program.cs
@@ -14,17 +14,18 @@ namespace Linq_XML_Sort_Demo
             XElement xelement = XElement.Load(@"C:\Users\vishal.rathore\source\repos\Day20\ELibrary.xml");
             var dict = (from element in xelement.Descendants("book")
                         let name = (string)element.Attribute("author")
+                        let title = (string)element.Element("Page")
                         orderby name
                         select new
                         {
 
-                            Title = element.Attribute("title").Value,
+                            Title = title,
                             AuthorName = name
                         }).ToDictionary(c => c.Title, c => c.AuthorName);
 
             foreach(var item in dict)
             {
-                Console.WriteLine("Book's Title : {0}  ||  Author : {1}",item.Key,item.Value);
+                Console.WriteLine("Book's Page : {0}  ||  Author : {1}",item.Key,item.Value);
                 Console.WriteLine();
                 Console.WriteLine("-------------------------");
                 Console.WriteLine();
