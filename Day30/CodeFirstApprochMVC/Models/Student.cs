eb111b5071d566b4176bc74104993a693100636f
vishal-gl <vishal.rathore@globallogic.com>
2022-08-26 08:21:58 +0530

    CodeFirstApproach

---
+++ Day30/CodeFirstApprochMVC/Models/Student.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace CodeFirstApprochMVC.Models
+{
+    [Table("Student_Table")]
+    public class Student
+    {
+        [Key]
+        public int SId { get; set; }
+
+
+        [Required(ErrorMessage ="Name is Required")]
+        [StringLength(40,ErrorMessage="Length of Name Should not be More than 40 character")]
+        
+        [MinLength(3,ErrorMessage ="Name Should have Minimum 3 character")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Address is Required")]
+        [MinLength(3, ErrorMessage = "Address Should have Minimum 3 character")]
+        public string Address { get; set; }
+
+        
+        [Required(ErrorMessage ="Date Of Birth Is Required")]
+        [DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
+
+
+
+        [Required(ErrorMessage = "Age Is Required")]
+        [Range(3,32, ErrorMessage="Age Should be lie between 3 and 32")]
+        public int Age { get; set; }
+
+
+        [Required(ErrorMessage = "Email is Required")]
+        
+        [MinLength(3, ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Password Is Required")]
+        [MinLength(3,ErrorMessage ="Invalid Password")]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "Confirm Password Is Required")]
+        [Compare("Password",ErrorMessage ="Password does NOT match")]
+        public string CPassword { get; set; }
+
+
+
+
+
+
+    }
+
+    public class DB : DbContext
+    {
+        public DB() : base("cs")
+        {
+
+        }
+        public DbSet<Student> Students
+        {
+            get;set;
+        }
+
+
+    }
+}
